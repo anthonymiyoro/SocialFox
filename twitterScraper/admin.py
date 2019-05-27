b322900b3029d4461a95c88df01a2f7d021a0078
anthonymiyoro <amiyoro@gamil.com>
2019-05-27 08:56:27 +0000

    Created model and added to admin

--- twitterScraper/admin.py
+++ twitterScraper/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Tweets
 # Register your models here.
+
+class TweetsAdmin(admin.ModelAdmin):
+    fields = ['tweet', 'tweet_id', 'date_collected', 'twitter_user', 'number_of_likes', 'number_of_retweets', 'tweet_device']
+
+admin.site.register(Tweets, TweetsAdmin)
--- twitterScraper/models.py
+++ twitterScraper/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+# This model stores Tweets collected
+class Tweets(models.Model):
+    tweet_id = models.CharField()
+    tweet = models.CharField()
+    date_collected = models.DateTimeField(auto_now_add=True)
+    twitter_user = models.CharField("User Name on Twitter")
+    number_of_likes = models.IntegerField()
+    number_of_retweets = models.IntegerField()
+    tweet_device = models.CharField("Device Tweet was collected from", blank=True, null=True)
+
+
+
+
+
