9406c6b1851a703a6f50f802500e222e0f868d3b
anthonymiyoro <amiyoro@gamil.com>
2019-05-27 13:01:19 +0000

    Added detailed admin views

--- twitterScraper/admin.py
+++ twitterScraper/admin.py
@@ -5,5 +5,7 @@ from .models import Tweet
 
 class TweetAdmin(admin.ModelAdmin):
     fields = ['tweet', 'tweet_id', 'date_collected', 'twitter_user', 'number_of_likes', 'number_of_retweets', 'tweet_device']
+    list_display = ('tweet', 'tweet_id', 'date_collected', 'twitter_user', 'number_of_likes', 'number_of_retweets', 'tweet_device')
+    list_filter = ('tweet_id', 'date_collected', 'number_of_likes', 'number_of_retweets')
 
 admin.site.register(Tweet, TweetAdmin)
